c0a7d51ed76ea131837f1cd23feb14da453d5ca5
Igor Milosavljevic <migor@live.co.uk>
2025-04-06 11:58:39 +0100

    handler & router boilerplate

---
+++ server/src/handlers/user.ts
@@ -0,0 +1,4 @@
+import { Request, Response } from 'express';
+
+export const createUser = async (req: Request, res: Response) => {};
+export const loginUser = async (req: Request, res: Response) => {};
