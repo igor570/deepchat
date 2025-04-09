e1347a8d659d7f071930f3dd2cded1d01d42ea71
Igor Milosavljevic <migor@live.co.uk>
2025-04-09 21:43:45 +0100

    simplified ai response

--- server/src/utils/generatePrompt.ts
+++ server/src/utils/generatePrompt.ts
@@ -5,7 +5,7 @@ export const generatePrompt = async (userMessage: string) => {
     model: 'gemini-2.0-flash',
     contents: userMessage,
   })
-  const message = reply.candidates?.[0]?.content?.parts?.[0]?.text
+  const message = reply.text
 
   if (!message) return 'No response received from AI Agent'
 
