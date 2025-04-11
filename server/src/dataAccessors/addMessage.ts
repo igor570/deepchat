043bae9bbd97cf7dd7f264af2c88ec0280f482a0
Igor Milosavljevic <migor@live.co.uk>
2025-04-11 10:24:12 +0100

    remove logs

--- server/src/dataAccessors/addMessage.ts
+++ server/src/dataAccessors/addMessage.ts
@@ -1,18 +1,13 @@
 import db from '../db'
 import { Message } from '../types/message'
 
-/*
-TODO: Make a function that takes the user's message from the socket 
-      and inserts it into the messages table
-*/
-
 export const addMessage = async ({ userId, content, senderType }: Message) => {
-    if (!userId || content || senderType) return
+    if (!userId || !content || !senderType) return
 
     try {
         await db.query(
             `INSERT INTO messages (user_id, content, sender_type) VALUES ($1, $2, $3)`,
-            [userId, content, senderType],
+            [userId, content, senderType]
         )
 
         console.log('Added message to DB')
