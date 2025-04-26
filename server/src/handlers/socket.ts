6c8c79cda6cc71aea4cecc24b2d7b1ef20c06805
Igor Milosavljevic <migor@live.co.uk>
2025-04-26 14:26:15 +0100

    changed socket behaviour on server

--- server/src/handlers/socket.ts
+++ server/src/handlers/socket.ts
@@ -4,22 +4,26 @@ import { aiUUID } from '../utils/consts'
 import { generatePrompt } from '../utils/generatePrompt'
 import { ExtendedError } from 'socket.io/dist/namespace'
 import { CustomSocket } from '../types/customSocket'
+import { Socket } from 'socket.io'
 
 //JWT Verification Socket Middleware
 export const authenticateSocket = (
-    socket: CustomSocket,
+    socket: Socket,
     next: (err?: ExtendedError) => void
 ) => {
     const token = socket.handshake.auth.token
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET ?? '')
-        if (typeof payload !== 'string' && 'userId' in payload) {
-            socket.userId = payload.userId
-            console.log(`User connected: ${socket.userId}`)
-            next()
-        } else {
-            throw new Error('Invalid token payload')
-        }
+
+        //TODO: Fix this auth to receive userId correctly so the auth middlware works
+
+        // if (typeof payload !== 'string' && 'userId' in payload) {
+        //     socket.userId = payload.userId
+        //     console.log(`User connected: ${socket.userId}`)
+        //     next()
+        // } else {
+        //     throw new Error('Invalid token payload')
+        // }
     } catch (err) {
         console.log('Authentication failed:', err)
         next(new Error('Authentication error'))
@@ -27,10 +31,11 @@ export const authenticateSocket = (
 }
 
 // chatHandler.ts
-export const handleChatMessage = (socket: CustomSocket) => {
-    return async (msg: string) => {
+export const handleChatMessage = (socket: Socket) => {
+    return async (message: { userId: string; message: string }) => {
         try {
-            const userId = socket.userId
+            const userId = message.userId
+            const msg = message.message
 
             //Add user message to DB
             await addMessage({
